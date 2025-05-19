814055f32b2d81e3dda8e86b64fd817ca5a91235
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-19 16:43:30 +0300

    Add control flow example

---
+++ examples/control_flow.py
@@ -0,0 +1,14 @@
+grade = 8
+
+if grade:
+    print("Grade is non-zero (truthy).")
+
+if grade >= 5:
+    print("Congrats! You passed!")
+    print("still under if")
+
+print("outside if")
+
+
+for nr in range(10, 15):
+    print(nr)
--- examples/operators.py
+++ examples/operators.py
@@ -11,3 +11,11 @@ if is_student is True:
 # checks if is_student is ANY truthy value
 if is_student:
     print("3. Person is student.")
+
+
+age = None
+age = age or 0
+
+age = 0 if age is None else age  # ternary operator
+# other languages:  cond ? val_if_true : val_if_false
+# Python: val_if_true if cond else val_if_false
