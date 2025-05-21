54d27f334f63418e040515513f937ef4405f3bce
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-21 10:24:03 +0300

    Add LEGB rule example

---
+++ examples/legb.py
@@ -0,0 +1,40 @@
+X = 100
+
+
+def func(a):
+    b = "b"
+    # sum = 0  # shadowing of built-in name
+    # X = 0  # shadowing of global name
+
+    def inner(c):
+        d = "d"
+        # a = "test"
+        # X = "test"
+        print("--- local inner() ---")
+        print("Local names:", c, d)
+        print("Enclosing names:", a, b, inner)
+        print("Global names:", X, func, MyClass)
+        print("Built-in names:", sum, min, list, dict, end="\n\n")
+
+    inner("c")
+
+    print("--- local func() ---")
+    print("Local names:", a, b, inner)
+    print("Global names:", X, func, MyClass)
+    print("Built-in names:", sum, min, list, dict, end="\n\n")
+
+
+class MyClass:
+    pass
+
+
+# Shadowing of built-in names (should be avoided)
+# list = []
+# def min(x, y):
+#     return x
+
+func("a")
+
+print("--- global ---")
+print("Global names:", X, func, MyClass)
+print("Built-in names:", sum, min, list, dict)
